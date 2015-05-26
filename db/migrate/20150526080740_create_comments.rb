1cd304bd8437ce207b7711a83cef71012c50009d
Reinhardt Lin <reinhardt.lin@gmail.com>
2015-05-26 16:39:49 +0800

    Generate fake data

--- db/migrate/20150526080740_create_comments.rb
+++ db/migrate/20150526080740_create_comments.rb
@@ -3,9 +3,11 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.text :content
       t.integer :topic_id
+      t.integer :user_id
 
       t.timestamps
     end
     add_index :comments, :topic_id
+    add_index :comments, :user_id
   end
 end
