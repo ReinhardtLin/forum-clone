16f9fae98153de328ba9e2c47180f085ea49cdaf
Reinhardt Lin <reinhardt.lin@gmail.com>
2015-05-27 14:17:37 +0800

    add user profile page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic, :counter_cache => true
   validates_presence_of :content
+
+  def title
+    self.topic.title
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :topics do
     resources :comments
   end
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
