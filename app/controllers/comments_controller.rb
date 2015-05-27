299283f550e3b59f6818b54a3512190508d875d4
Reinhardt Lin <reinhardt.lin@gmail.com>
2015-05-27 13:25:44 +0800

    create topic and reply comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @topic = Topic.find(param[:topic_id])
+    @comment = @topic.comments.build( comment_params )
+    @comment.user = current_user
+    if @comment.save
+      redirect_to topic_path(@topic)
+    else
+      render "/topics/show"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,32 @@
 class TopicsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @topics = Topic.page(param[:page]).per(10)
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    @topic.user = current_user
+
+    if @topic.save
+      redirect_to topics_url
+    else
+      render :action => :new
+    end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:title, :content)
   end
 end
